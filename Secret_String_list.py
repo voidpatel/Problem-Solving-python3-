7f696c63072391d248559b59184cafe94ac70a4c
voidpatel <33804927+voidpatel@users.noreply.github.com>
2020-04-06 16:00:02 -0700

    Update Secret_String_list.py

--- Secret_String_list.py
+++ Secret_String_list.py
@@ -1,3 +1,7 @@
+# receive a string(Uppercase, lowercas and spaces) and then hide its meaning by turning it into a string of unicodes
+# then translate it from unicode back into its original meaning using lists
+# unicodes a-z: 65-90, A-Z: 97-122, space( ): 32
+
 samp_string = input('Enter a string: ')
 
 # create list containg unicodes of each character in sample string
