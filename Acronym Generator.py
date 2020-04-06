ffbb4d43d82c6bd3fbf5806d33242475f2ca49d1
voidpatel <33804927+voidpatel@users.noreply.github.com>
2020-04-06 14:24:42 -0700

    Update Acronym Generator.py

--- Acronym Generator.py
+++ Acronym Generator.py
@@ -1,16 +1,20 @@
+#Input string to get its acronym
 rand_string = input("Enter a string: ")
 
+#Empty string to save the acronym
 acro_string = ""
 
+#Convert all the letters to uppercase
 cap_string = rand_string.upper()
 
-
+#Split whole string to individual charaters and save in new list
 first_list = cap_string.split()
 
+#Loop through the list and save the first character in empty string
 for i in first_list:
     acro_string += i[0]
-    
-print("Afcronym of given string: ", acro_string)
+   
+print("Acronym of given string: ", acro_string)
          
 
      
