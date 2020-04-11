87a42c38cfe7037cd4cc646d9462325001fad550
Neel Patel <33804927+voidpatel@users.noreply.github.com>
2020-04-11 12:17:21 -0700

    Update and rename Employee_deict(Exception_handling).py to Employee_dict(Exception_handling).py

--- Employee_dict(Exception_handling).py
+++ Employee_dict(Exception_handling).py
@@ -8,21 +8,27 @@ while True:
     try:
         
         ans = input("Add customers (Yes/No): ")
+        # convert the input to lowercase
         ans = ans.lower()
        
+        # only accept input with length 1 or 3
         if len(ans) != 1 and len(ans) != 3:
             print("Please enter y, yes, n or no")
             break
         
+        # if yes, take input and check for value errors of both first and last name
         elif ans == 'y' or ans == 'yes':
             fname, lname = input("Enter Employee Name : ").split()
             len(fname) > 0
             len(lname) > 0
+            # append data if inputs are correct
             employees.append({'fname': fname, 'lname': lname})
-
+        
+        # exit the function if input is no
         elif ans == 'n' or ans == 'no':
             break
-    
+            
+    # print error message incase of value error
     except ValueError: 
         print("Enter both first and last name")
         
