2ce88ed8c36d1bb502a41867c4327b7212002bba
Neel Patel <33804927+voidpatel@users.noreply.github.com>
2020-04-12 17:25:37 -0700

    Add files via upload

---
+++ Files(read,write).py
@@ -0,0 +1,25 @@
+# write and read from a file
+
+# wrtie on a file
+with open("mydata.txt", mode="w", encoding="utf-8") as myfile:
+    
+    myfile.write("Neel\nPatel\nNileshbhai")
+ 
+# read individual lines from a file
+with open("mydata.txt", encoding="utf-8") as myfile:
+    
+    line_num = 1
+    
+    # loop unitl line ends
+    while True:
+        
+        data = myfile.readline()
+        
+        # if line doesn't return data, breal the loop
+        if not data:
+            break
+        
+        print("Line Number ", line_num, ":", data, end="")
+        
+        line_num += 1
+        
